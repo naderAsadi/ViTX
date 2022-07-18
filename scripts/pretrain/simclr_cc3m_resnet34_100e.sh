e6855526a99f65f6391cf6a6b88650d58cf79ede
naderasadi <asadi.nader97@gmail.com>
2022-07-18 19:24:31 -0400

    :paperclip: scripts updated

--- scripts/pretrain/simclr_cc3m_resnet34_100e.sh
+++ scripts/pretrain/simclr_cc3m_resnet34_100e.sh
@@ -1,5 +1,5 @@
 python main.py method="simclr" \
-data="cc3m" data/transform="simclr" data.transform.n_views=2 data.n_workers=12 \
+data="cc3m" data/transform="simclr" data.transform.n_views=2 data.n_workers=16 \
 model/vision_model="resnet34" model.vision_model.embed_dim=512 model/optimizer="sgd" \
 model.optimizer.scheduler.milestones="30-50-70" \
 train.batch_size=512 train.n_epochs=100 train.mixed_precision=True \
